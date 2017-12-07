814b504817fdf62cdd9510553bbe4f13781e9f62
css <2213506121@qq.com>
2017-12-07 17:46:29 +0800

    一二级混合菜单

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,25 +1,27 @@
 $(function () {
     $("#side-menu>li>b").next().find("li").slideUp();
-    //左侧导航
+    //左侧一二级混合菜单导航
 
     //展开收起
-    $("#side-menu>li").on("click","b",function () {
-        //复杂方法
-        /*if($(this).hasClass("up")){
-            $(this).removeClass("up");
-            $(this).next().find("li").slideUp();
-        }else{
-            $(this).next().find("li").slideDown();
-            $(this).addClass("up");
-        }*/
-        //简单方法
-        $(this).next().find("li").slideToggle();
-        $(this).toggleClass("up");
+    $("#side-menu>li").on("click",function () {
+        $(this).find("li").slideDown();
+        $(this).find("b").addClass("up");
+        $(this).siblings().find("b").removeClass("up");
+        $(this).siblings().find(".side-list>li").slideUp();
     });
 
     //选中
     $(".side-list>li").on("click","a",function () {
         $(this).parent().addClass("active").siblings().removeClass("active");//点击项加上active,兄弟项去掉active;
         $(this).parents(".level1").siblings().find("li").removeClass("active");//旁系兄弟项也去掉active;
+        $(this).parents("#side-menu").find("li").eq(0).removeClass("active");//第一项一级菜单特殊处理
+
     });
+
+    //第一项（特殊处理）
+    $("#side-menu>li:eq(0)").on("click","a",function () {
+        $(this).parent().addClass("active");
+        $(this).parents(".level1").siblings().find("li").removeClass("active");//旁系兄弟项也去掉active;
+    });
+
 });
